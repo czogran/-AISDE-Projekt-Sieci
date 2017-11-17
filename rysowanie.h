c316f9981bf505b39db1c2d8b478a18ed068b90f
Jakub Brzozowski <brzozopoland@gmail.com>
2017-11-18 00:42:36 +0100

    Funkcje rysowanie węzła i krawędzi

---
+++ rysowanie.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <iostream>
+#include <Windows.h>
+#include "graphics.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+void rysuj_wezel(int x, int y)
+{
+	int gdriver = 0, gmode, errorcode;
+	initgraph();
+	setcolor(7);
+
+	circle(x + 30, y + 30, 30); // jest x,y + 30 zeby nie wchodziło na tekst w konsoli
+}
+
+void rysuj_krawedz(int x1, int y1, int x2, int y2)
+{
+	int gdriver = 0, gmode, errorcode;
+	initgraph();
+	setcolor(7);
+
+	line(x1 + 30, y1 +30, x2 + 30, y2 + 30);
+}
