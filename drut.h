925d065502278a07c3a7359efde9191b9f0052d3
czogran <pawel.rawicki@gmail.com>
2018-01-07 00:53:19 +0100

    segregacja kabli 1:0

--- drut.h
+++ drut.h
@@ -13,12 +13,14 @@ public:
 	int pojemnosc;
 	int koszt_jednostkowy;
 	float oplacalnosc;
+	float cena_za_wiazke;
 	Drut(int nr, int pojemnosc, int koszt_jednostkowy)
 	{
 		nr = numer;
 		this->pojemnosc = pojemnosc;
 		this->koszt_jednostkowy = koszt_jednostkowy;
-		oplacalnosc = pojemnosc / koszt_jednostkowy;
+		oplacalnosc =  pojemnosc/koszt_jednostkowy;
+		cena_za_wiazke = koszt_jednostkowy / pojemnosc;
 	}
 
 };
--- rozszerzenie.h
+++ rozszerzenie.h
@@ -6,7 +6,94 @@
 #include "wezel.h"
 #include "sciezka.h"
 #include "krawendz.h"
+#include"drut.h"
+#include<algorithm>
+#include<math.h>
 
+bool myfunction(Drut i, Drut j) { return (i.cena_za_wiazke < j.cena_za_wiazke); }// moze vyc >??? na czuja
+
+float wiazka(vector<Drut> druty,Wezel wezel)
+{
+	 float wiazka=INFINITE;
+	 vector<int>tablica;
+	 vector<float> koszt_alternatywny;
+	 //const int a=druty.size();
+	
+	// int tablica[11];
+
+	 //int koszt_alternatywny=INFINITE;
+	// int koszt_dopelnienia = 100000;
+	 int klijenci;
+	 int indeks = 0;
+	 klijenci = wezel.klijenci;
+	 sort(druty.begin(), druty.end(), myfunction);
+	 tablica.push_back( floor(klijenci / druty[indeks].pojemnosc));
+	 koszt_alternatywny.push_back( druty[indeks].koszt_jednostkowy*tablica[indeks]);
+	 klijenci=klijenci - tablica[indeks] * druty[indeks].pojemnosc;
+	 indeks++;
+	 while (klijenci > 0)
+	 {
+		 if (druty.size() > indeks) {
+
+			 tablica.push_back(floor(klijenci / druty[indeks].pojemnosc));
+
+			 koszt_alternatywny.push_back(tablica[indeks] * druty[indeks].koszt_jednostkowy);
+			 klijenci = klijenci - tablica[indeks] * druty[indeks].pojemnosc;
+			 indeks++;
+		 }
+		 else
+		 {
+			 tablica.push_back(floor(klijenci / druty[indeks].pojemnosc) );
+			
+			 klijenci = klijenci - tablica[indeks] * druty[indeks].pojemnosc; 
+			 if (klijenci != 0)
+				 tablica[indeks]++;
+			 koszt_alternatywny.push_back(tablica[indeks] * druty[indeks].koszt_jednostkowy);
+		 }
+		 
+	 }
+	//wiazka = koszt_alternatywny[0];
+	 //int min=INFINITE;
+	 float suma = 0;//koszt_alternatywny[0];
+	 for (int i = 0;i < koszt_alternatywny.size()-1;i++)
+	 {
+		 suma = suma + koszt_alternatywny[i];
+		 if (wiazka > suma + druty[i].koszt_jednostkowy)
+			 wiazka = suma + druty[i].koszt_jednostkowy;
+
+		cout << "\n" << suma;
+	 }
+	 suma = suma + koszt_alternatywny[koszt_alternatywny.size()-1];
+	 if (wiazka > suma)
+		 wiazka = suma ;
+
+	 cout << "\nsuma to:" << suma;
+
+	 /*while (klijenci > 0)
+	 {
+		 tablica = floor(klijenci / druty[indeks].pojemnosc);
+		 klijenci = klijenci - druty[indeks].pojemnosc * tablica;
+		 if (koszt_alternatywny < tablica*druty[indeks].pojemnosc)
+		 {
+			 wiazka = wiazka + koszt_alternatywny;
+
+		 }
+		 if (koszt_alternatywny > (tablica + 1)*druty[indeks].pojemnosc)
+		 {
+				if (klijenci>0)
+				 koszt_alternatywny = (tablica + 1)*druty[indeks].pojemnosc);
+				else
+				{
+					wiazka = wiazka + tablica *druty[indeks].pojemnosc);
+					break;
+				}
+		 }
+		
+	 }
+ */
+	 cout << "\n wiazka to: " << wiazka << "\n";
+	return wiazka;
+}
 
 Sciezka rozszerzenie(vector<Wezel> wezel, vector<Krawendz>krawendz, int start, int end,int koszt_drutu)
 {
